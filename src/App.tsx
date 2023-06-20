a8a569460a9d67323b21cb11397589f25b506f6a
혀느현스 <hyuns@hyuns.dev>
2023-06-21 04:13:23 +0900

    feat: 홈페이지 업데이트

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import "./App.css";
+
+import BlueSSULogo from "./assets/icon.png";
+
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #3045ff;
+    flex-direction: column;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    max-width: 200px;
+`;
+
+const Title = styled.h1`
+    color: #fff;
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 900;
+    font-size: 32px;
+`;
+
+const Description = styled.p`
+    color: #fff;
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+`;
+
+const URL = styled.p`
+    color: rgba(255, 255, 255, 0.5);
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+`;
+
+const GithubURL = styled.a`
+    color: rgba(255, 255, 255, 0.5);
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    text-decoration: none;
+
+    transition: 200ms;
+
+    &:hover {
+        text-decoration: underline;
+        color: rgba(255, 255, 255, 1);
+    }
+`;
+
+function App() {
+    return (
+        <Center>
+            <Image src={BlueSSULogo} alt="BlueSSU Logo" />
+            <Title>Blue SSU</Title>
+            <Description>숭실대학교 개발 프로젝트</Description>
+            <GithubURL href="https://github.com/blue-ssu" target="_blank">
+                https://github.com/blue-ssu
+            </GithubURL>
+        </Center>
+    );
+}
+
+export default App;
